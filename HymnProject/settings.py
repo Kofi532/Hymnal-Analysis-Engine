48a4b391284c218d6ed20d5d8c61fdd5f36315a1
kofi <kadukpo@gmail.com>
2026-03-22 08:01:42 +0000

    Fix: .

--- HymnProject/settings.py
+++ HymnProject/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = 'django-insecure-(s$anx32*h=0ryv22dzs-z3)gp%p4jdx$zeul-x6$5k^14jp2o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']  # Allows any host. Simple, but less secure for high-stakes apps.
 
 # Application definition
 
